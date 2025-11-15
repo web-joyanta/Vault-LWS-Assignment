26f0cb711f3740fb82408ce191d7519245c16e78
web-joyanta <web.joyanta@gmail.com>
2025-11-15 20:14:56 +0600

    vault lws assignment project setup: vite,react,tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold underline text-center">
+        welcome to vault lws assignment
+      </p>
+    </>
+  )
+}
+
+export default App
